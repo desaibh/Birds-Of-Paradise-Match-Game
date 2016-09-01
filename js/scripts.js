486ab2e59bd232c9c30613b8d4d55889d034a5e5
Bhavika Desai <desaibh@gmail.com>
2016-09-01 11:47:34 -0400

    submit Day 1 progress

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ let birdChoice;
 let cellClass;
 let cellId;
 let newChild;
+
 //Generate Cells on Board & Assign Display Value for Cell
 for (let i = 1; i < 101; i++) {
   birdPicker = Math.floor(Math.random()*4+1);
@@ -15,5 +16,14 @@ for (let i = 1; i < 101; i++) {
   document.querySelector('div #board').appendChild(newChild);
   document.getElementById(cellId).className = cellClass;
 }
+
+//Check Row Matches
+for (i = 1; i <=10; i++) {
+  if ()
+}
+
+
+// check column matches
+
 //Swap Cells
 for (i
